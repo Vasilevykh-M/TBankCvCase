61dfd0716d15f618b668613c378e56ed312a922b
Vasilevykh-M <vasilevyh00@gmail.com>
2024-12-11 16:50:26 +0500

    fix(worker image generator): fix input data in fast api

--- mgie_api/main.py
+++ mgie_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File, status
 from fastapi.middleware.cors import CORSMiddleware
 
 import io
@@ -6,6 +6,8 @@ import PIL
 import base64
 from typing import Dict
 from pydantic import BaseModel
+from io import BytesIO
+
 
 import uvicorn
 
@@ -36,20 +38,21 @@ app.add_middleware(
 
 
 @app.post("/generate/")
-async def generate(payload: ImageTextPayload):
-    try:
-        try:
-            image_data = base64.b64decode(payload.image_bytes)
-        except Exception as e:
-            raise HTTPException(status_code=400, detail="Invalid image bytes provided.")
-        
-        if not payload.text:
-            raise HTTPException(status_code=400, detail="Text field cannot be empty.")
+async def generate(img_file: UploadFile = File(...), promt: str = ""):
+    if all(ext not in img_file.filename for ext in ['.jpg', '.jpeg', '.png']):
+        return HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail=f'File {img_file.filename} has unsupported extension type',
+        )
 
-        pil_image = PIL.Image.open(io.BytesIO(image_data))
+    request_object_content = await img_file.read()
+    pil_image = PIL.Image.open(BytesIO(request_object_content))
+
+    try:
+
         pil_image.save("ex.jpg")
 
-        generated_result = await model.generate_image(pil_image, payload.text)
+        generated_result = await model.generate_image(pil_image, promt)
 
         byte_array = io.BytesIO()
         generated_result.save(byte_array, format='JPG')
